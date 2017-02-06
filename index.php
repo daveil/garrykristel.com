3e45b9bd5388e5ccbc4248b76839e173b8e168b7
Garry <garry.p@tripleonetech.com>
2017-02-06 16:05:35 +0800

    large map testing

--- index.php
+++ index.php
@@ -16,6 +16,6 @@
 	<h1>Garry & Kristel</h1>
 	<?php echo 'A day to remember. A promise of forever.';?>
 	<?php echo '</br>try! pogi ako';?>
-	<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d15446.813436344342!2d121.0162967!3d14.55894865!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sph!4v1486368186709" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3869.878374458344!2d121.13960641540977!3d14.084356290132948!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd6f86d4165eb5%3A0xc9096337504df69a!2sMabini+Ave%2C+Tanauan%2C+Batangas!5e0!3m2!1sen!2sph!4v1486368293259" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
 </body>
 </html>
