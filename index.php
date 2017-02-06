e526d853b7fb6a14fcb5a93b16e2756fed22030a
Garry <garry.p@tripleonetech.com>
2017-02-06 16:03:37 +0800

    try embed maps

--- index.php
+++ index.php
@@ -16,5 +16,7 @@
 	<h1>Garry & Kristel</h1>
 	<?php echo 'A day to remember. A promise of forever.';?>
 	<?php echo '</br>try! pogi ako';?>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d15446.813436344342!2d121.0162967!3d14.55894865!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sph!4v1486368186709" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 </body>
+https://www.google.com.ph/maps/@14.0843563,121.1396064,17z/data=!4m2!10m1!1e2?hl=en
 </html>
